dc2982a7c060bc0024d509171d27594aece488a6
TienPhuc02 <dtp2352002@gmail.com>
2024-02-20 13:51:18 +0700

    the POJO

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
-
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-//        Challenge1 ex1= new Challenge1();
-//        double result = ex1.SquareNumber(2,3,4);
-//        int IntegerValue =(int) result;
-//        System.out.println(IntegerValue);
 
-
-        //challeng2
-
-//        Challenge2 car =new Challenge2("Ferrari","F8","100");
-//        car.getAllData();
-
-
-
-        //challeng3
-
-        Challenge3 student = new Challenge3("Jack", 60 ,70);
-        System.out.println(student.calcTotal(student.getMark1(), student.getMark2()));
-
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Harry";
+                        case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "Java Masterclass");
+            System.out.println(s);
+        }
     }
 }
